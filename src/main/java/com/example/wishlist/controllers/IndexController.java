57ec49a429b99637cdc1ab8e0207c168fee9d3c7
Lauribee <laura_stroe_2001@yahoo.com>
2022-04-05 13:03:59 +0200

    class user med attributter, gettere og settere

---
+++ src/main/java/com/example/wishlist/controllers/IndexController.java
@@ -0,0 +1,61 @@
+package com.example.wishlist.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+    return "index";
+}
+    @GetMapping("/signup")
+    public String signup(){
+    return "signup";
+}
+    @GetMapping("/profile")
+    public String profile(){
+    return "profile";
+}
+
+    @GetMapping("/wishlist")
+    public String wishlist(){
+        return "wishlist";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/logged")
+    public String loggedindex(){
+        return "logged";
+    }
+
+    @GetMapping("/browse")
+    public String browse(){
+        return "browse";
+    }
+
+    @GetMapping("/category-beauty")
+    public String categoryBeauty(){
+        return "category-beauty";
+    }
+
+    @GetMapping("/category-clothes")
+    public String categoryClothes(){
+        return "category-clothes";
+    }
+
+    @GetMapping("/category-electronics")
+    public String categoryElectronics(){
+        return "category-electronics";
+    }
+
+    @GetMapping("/category-sport")
+    public String categorySport(){
+        return "category-sport";
+    }
+}
